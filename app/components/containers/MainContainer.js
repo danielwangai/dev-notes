7693452e437fb53c7d50e773a34d25bbdbaafc7c
Daniel Wangai <mainad57@gmail.com>
2018-01-19 15:38:00 +0300

    ch(webpack-hmr): - refactor webpack to enable hmr - add react-hot-loader

--- app/components/containers/MainContainer.js
+++ app/components/containers/MainContainer.js
@@ -5,7 +5,7 @@ import { Main } from '../pages/Main.js'
 class MainContainer extends Component {
   render () {
     return (
-      <div>Content</div>
+      <div>Content!!!</div>
     )
   }
 }
--- app/index.js
+++ app/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { applyMiddleware, createStore, combineReducers, compose } from 'redux'
 import { Provider } from 'react-redux'
+
 import thunk from 'redux-thunk'
+import { AppContainer } from 'react-hot-loader';
 
 // local imports
 import users from './redux/reducers/users.js'
 import getRoutes from './components/App'
+import MainContainer from './components/containers/MainContainer'
 
 const rootReducer = combineReducers({users})
 
@@ -15,9 +18,21 @@ const store = createStore(rootReducer, compose(
   window.devToolsExtension() ? window.devToolsExtension() : (f) => f
 ))
 
-ReactDOM.render(
-  <Provider store={store}>
-    {getRoutes()}
-  </Provider>,
-  document.getElementById('root')
-)
+const renderApp = (Component) => {
+  ReactDOM.render(
+    <Provider store={store}>
+      {Component}
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+renderApp(getRoutes())
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./components/App.js', () => {
+    const HotApp = require('./components/App.js').default
+    // render App on change
+    renderApp(HotApp)
+  })
+}
